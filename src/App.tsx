f79722799f69655863cf3a26c03329138bcfb7db
Brihat Thapa <brihatthapa7@gmail.com>
2025-12-18 16:55:38 +0545

    commit 3
    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import Hero from "./components/Hero";
 import "./index.css";
 import Navbar from "./components/Navbar";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import NewsSection from "./components/NewsSection";
 const App = () => {
   return (
     <>
       <Hero />
       <Navbar/>
+      <Home/>
+      <NewsSection/>
       <Footer/>
+      
     </>
   );
 };
