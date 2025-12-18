358efffbc3a26935098feb5a0f7b6a820f983348
Brihat Thapa <brihatthapa7@gmail.com>
2025-12-18 16:31:46 +0545

    footer changes
    changes to footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import Hero from "./components/Hero";
 import "./index.css";
 import Navbar from "./components/Navbar";
-<<<<<<< Updated upstream
-import NewsSection from "./components/NewsSection";
-=======
-import Home from "./components/home";
 import Footer from "./components/footer";
->>>>>>> Stashed changes
 const App = () => {
   return (
     <>
       <Hero />
       <Navbar/>
-<<<<<<< Updated upstream
-      <NewsSection/>
-=======
-      <Home/>
       <Footer/>
->>>>>>> Stashed changes
     </>
   );
 };
