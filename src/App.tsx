938aa32118278fa7fa0de51c3761a9e266a351ad
aavashgmre <aavashgmre@gmail.com>
2025-12-18 16:26:00 +0545

    fourth commit
    added newssection

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import "./index.css";
 import Navbar from "./components/Navbar";
+import NewsSection from "./components/NewsSection";
 const App = () => {
   return (
     <>
       <Hero />
       <Navbar/>
+      <NewsSection/>
     </>
   );
 };
